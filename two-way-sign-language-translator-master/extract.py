9e6d178c6990558c3ba5c80bb68695ce7a26e94d
chirag <chiragsurendra2002@gmail.com>
2024-10-25 11:16:34 +0530

    project pushed

---
+++ two-way-sign-language-translator-master/extract.py
@@ -0,0 +1,22 @@
+import os
+import shutil
+def getListOfFiles(dirName):
+    listOfFile = os.listdir(dirName)
+    allFiles = list()
+    for entry in listOfFile:
+        fullPath = os.path.join(dirName, entry)
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
+        else:
+            if(fullPath.endswith('.webp')):
+                allFiles.append(fullPath)
+                
+    return allFiles
+
+dirname = 'C:\\Users\\Asus\\OneDrive\\Desktop\\two-way-sign-language-translator-master'
+dest="C:\\Conv"
+
+data=getListOfFiles(dirname)
+for i in range(len(data)):
+    fname=dest+str(i)+".webp"
+    shutil.copyfile(data[i], fname)
